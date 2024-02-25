bf0b8f66e1a74dd79fd867ed15075c0d0ef6ff81
Richard Cândido <rich_dacan@protonmail.com>
2024-02-25 18:22:35 -0300

    feat :sparkles: set GlobalError in api

--- backend/src/controllers/amazon.ts
+++ backend/src/controllers/amazon.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { scrapAmazonService } from "../services/amazon";
+import GlobalError from "../errors/GlobalError";
 
 export const scrapAmazon = async (req: Request, res: Response) => {
   const keyword: string = req.query.keyword as string;
@@ -7,8 +8,7 @@ export const scrapAmazon = async (req: Request, res: Response) => {
   try {
     const products = await scrapAmazonService(keyword);
     res.json(products);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error });
+  } catch (error: unknown) {
+    throw new GlobalError((error as Error).message, 500);
   }
 };
